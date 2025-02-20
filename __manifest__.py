46db20213fd27a7d8c79cc991c9e2aa32de89f50
Nicolas Llancaqueo <n.llancaqueo01@ufromail.cl>
2025-02-20 04:25:55 -0300

    actualizacion modulo se implementa cantidad de productos y precio unitario a single product

--- __manifest__.py
+++ __manifest__.py
@@ -3,11 +3,12 @@
     'version': '1.0',
     'summary': 'Calcula el precio por unidad basado en la cantidad por caja',
     'author': 'Nicolás',
-    'category': 'Product',
-    'depends': ['product'],
+    'category': 'eCommerce',
+    'depends': ['product', 'website_sale'],
     'data': [
         'security/ir.model.access.csv',
         'views/product_template_view.xml',
+        'views/product_sitioweb_view.xml'
     ],
     'installable': True,
     'application': False,
